3555558449dcc7bd54f4be472aad1ee444748c23
Anoux Jadur <ajadur96@gmail.com>
2021-05-04 13:44:04 -0300

    changes in main.py

--- main.py
+++ main.py
@@ -15,19 +15,14 @@ ITEMS_SUPER = [
 
 STRING = "aslkjdhflkajshdflkajhdflkajshd"
 
-# @app.get("/")
-# def read_root():
-#     return {"Hello": "World"}
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
 
 @app.get("/items")
 def get_items():
 	print("Estoy en items")
 	return ITEMS_SUPER
-	
-# @app.get("/items/birra")
-# def get_item():
-	# print("Estoy en items")
-	# return ITEMS_SUPER["birra"]
 
 @app.get("/items/{item_id}")
 def get_item(item_id):
@@ -38,18 +33,31 @@ def get_item(item_id):
 
 
 
-
-
-
+# THIRD VERSION
 @app.get("/solve")
-def solve_problem(q: Optional[str] = []):
-	unordered_list = []	
-	for letter in q:
-		unordered_list.append(letter)
-	unordered_list.sort()
-	print(unordered_list)
-	return unordered_list
+def solve_problem(q: Optional[str]):
+	return {"solution": "".join(sorted(q))}
 
+# SECOND VERSION
+#@app.get("/solve")
+#def solve_problem(q: Optional[str] = []):
+#	unordered_list = []	
+#	for letter in q:
+#		unordered_list.append(letter)
+#	unordered_list.sort()
+#	solved_string = ("".join(unordered_list))
+#	solution = {"solution":solved_string}
+#	return solution
+
+# FIRST VERSION
+#@app.get("/solve")
+#def solve_problem(q: Optional[str] = []):
+#	unordered_list = []	
+#	for letter in q:
+#		unordered_list.append(letter)
+#	unordered_list.sort()
+#	print(unordered_list)
+#	return unordered_list
 
 # @app.get("/items/{item_id}")
 # def read_item(item_id: int, q: Optional[str] = None):
